f82dc0ec1b79f18580135fd75e2991febf73cfd0
Thomas Basler <thomas@familie-basler.net>
2025-02-08 00:41:16 +0100

    Move all display related initialization inside the display class

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,6 @@ void setup()
         MessageOutput.print("migrated... ");
         Configuration.migrate();
     }
-    auto& config = Configuration.get();
     MessageOutput.println("done");
 
     // Read languate pack
@@ -94,7 +93,6 @@ void setup()
     } else {
         MessageOutput.print("using default config ");
     }
-    const auto& pin = PinMapping.get();
     MessageOutput.println("done");
 
     // Initialize Network
@@ -134,20 +132,7 @@ void setup()
 
     // Initialize Display
     MessageOutput.print("Initialize Display... ");
-    Display.init(
-        scheduler,
-        static_cast<DisplayType_t>(pin.display_type),
-        pin.display_data,
-        pin.display_clk,
-        pin.display_cs,
-        pin.display_reset);
-    Display.setDiagramMode(static_cast<DiagramMode_t>(config.Display.Diagram.Mode));
-    Display.setOrientation(config.Display.Rotation);
-    Display.enablePowerSafe = config.Display.PowerSafe;
-    Display.enableScreensaver = config.Display.ScreenSaver;
-    Display.setContrast(config.Display.Contrast);
-    Display.setLocale(config.Display.Locale);
-    Display.setStartupDisplay();
+    Display.init(scheduler);
     MessageOutput.println("done");
 
     // Initialize Single LEDs
