1a33a6fc3d677ff28db40211e6efb0e3c60f4a01
Thomas Basler <thomas@familie-basler.net>
2025-02-13 22:50:08 +0100

    Move SpiManager registration into PinMapping class

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,6 @@
 #include "defaults.h"
 #include <Arduino.h>
 #include <LittleFS.h>
-#include <SpiManager.h>
 #include <TaskScheduler.h>
 #include <esp_heap_caps.h>
 
@@ -34,12 +33,6 @@ void setup()
 {
     // Move all dynamic allocations >512byte to psram (if available)
     heap_caps_malloc_extmem_enable(512);
-
-    // Initialize SpiManager
-    SpiManagerInst.register_bus(SPI2_HOST);
-#if SOC_SPI_PERIPH_NUM > 2
-    SpiManagerInst.register_bus(SPI3_HOST);
-#endif
 
     // Initialize serial output
     Serial.begin(SERIAL_BAUDRATE);
